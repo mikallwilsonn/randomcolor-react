098ae41e1fb8515757b8c256cc6c507e953fbf33
Michael Wilson <info@michaelwilsion.io>
2019-02-22 15:58:06 -0500

    Completed the initial setup for app

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+// ----
+// Dependencies
+import React, { Component } from 'react';
+
+
+// ----
+// Child Components
+
+
+// ----
+// App class
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+
+// ----
+// Export
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+// ----
+// Dependencies
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './theme/theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ----
+// Component
+import App from './components/App';
+
+
+// ----
+// Reducers
+import reducers from './reducers';
+
+
+// ----
+// Store
+const store = createStore( reducers );
+
+
+// ----
+// Render the appplication
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector( '#root' )
+);
