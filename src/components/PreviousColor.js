77cf22a59faf086f74866c42c731deda138542bc
Michael Wilson <mikallwilsonn@gmail.com>
2019-04-05 16:25:40 -0400

    Made some misc UI updates

--- src/components/PreviousColor.js
+++ src/components/PreviousColor.js
@@ -13,13 +13,13 @@ class PreviousColor extends Component {
             <div className="card col-auto previous-color p-0 m-2">
                 <div className="col-auto" style={{ backgroundColor: value, height: '125px' }} >&nbsp;</div>
                 <div className="card-body col-lg-12 row justify-content-between align-items-center p-3 m-0 text-center flex-nowrap">
-                    <small className="card-text m-0 text-center">{value}</small>
+                    <small className="card-text m-0 text-center">
+                        <code className="text-theme-dark-faded">{value}</code>
+                    </small>
 
                     <div class="dropdown">
                         <button className="dropdown-toggle bg-light text-secondary" type="button" id={`dropdown-${value}`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="352" height="448" viewBox="0 0 352 448">
-                                <path fill="#CCC" d="M96 184v48c0 13.25-10.75 24-24 24h-48c-13.25 0-24-10.75-24-24v-48c0-13.25 10.75-24 24-24h48c13.25 0 24 10.75 24 24zM224 184v48c0 13.25-10.75 24-24 24h-48c-13.25 0-24-10.75-24-24v-48c0-13.25 10.75-24 24-24h48c13.25 0 24 10.75 24 24zM352 184v48c0 13.25-10.75 24-24 24h-48c-13.25 0-24-10.75-24-24v-48c0-13.25 10.75-24 24-24h48c13.25 0 24 10.75 24 24z"></path>
-                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448 294.2v-76.4c0-13.3-10.7-24-24-24H286.2V56c0-13.3-10.7-24-24-24h-76.4c-13.3 0-24 10.7-24 24v137.8H24c-13.3 0-24 10.7-24 24v76.4c0 13.3 10.7 24 24 24h137.8V456c0 13.3 10.7 24 24 24h76.4c13.3 0 24-10.7 24-24V318.2H424c13.3 0 24-10.7 24-24z"/></svg>
                         </button>
 
                         <div className="dropdown-menu" aria-labelledby={`dropdown-${value}`}>
